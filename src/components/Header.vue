092983ad99c948d50052572a3218d56f592ac380
Edoardo <ercolino.edoardo@gmail.com>
2021-09-20 15:30:10 +0200

    Scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+      <img src="../assets/img/dc-logo.png" alt="">
+
+      <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#">COMICS</a></li>
+          <li><a href="#">MOVIE</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
